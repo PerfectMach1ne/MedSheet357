8b3b912203b5f2d8cafc9991ec3fb662937fdcad
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2022-07-13 23:48:34 +0200

    I hate git so much

--- main.py
+++ main.py
@@ -7,3 +7,4 @@ print(sh.sheet1.get('A1'))
 print(sh.sheet1.get('C27:E31'))
 print(sh.sheet1.get('Y53:Y57'))
 # I want to weep like a gorilla
+# I made this comment just to show a middle finger to git
