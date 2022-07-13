7b02b4903ba1654e92169cb6653146cef6227af7
PerfectMach1ne <75753483+PerfectMach1ne@users.noreply.github.com>
2022-07-13 23:44:20 +0200

    I made something work

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import gspread
 
-print("test")
+gc = gspread.oauth()
+
+sh = gc.open("The Plan")
+print(sh.sheet1.get('A1'))
+print(sh.sheet1.get('C27:E31'))
+print(sh.sheet1.get('Y53:Y57'))
+# I want to weep like a gorilla
